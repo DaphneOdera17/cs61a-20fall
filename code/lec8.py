792846c8e81898cc31e7dffa947eaa8b9c100293
Birdy <2944006086@qq.com>
2024-01-31 21:59:14 +0800

    update

--- code/lec8.py
+++ code/lec8.py
@@ -18,4 +18,89 @@ def fact(n):
         return 1
     else:
         return n * fact(n - 1)
-    
+
+def luhn_sum(n):
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return luhn_sum_double(all_but_last) + last
+
+def luhn_sum_double(n):
+    all_but_last, last = split(n)
+    luhn_digit = sum_digits(2 * last)
+    if n < 10:
+        return luhn_digit
+    else:
+        return luhn_sum(all_but_last) + luhn_digit
+
+def cascade(n):
+    """
+    :param n:
+    :return:
+    >>> cascade(321)
+    321
+    32
+    3
+    32
+    321
+    """
+    if n < 10:
+        print(n)
+    else:
+        print(n)
+        cascade(n // 10)
+        print(n)
+
+cascade(321)
+
+
+def inverse_cascade(n):
+    grow(n)
+    print(n)
+    shrink(n)
+
+def f_then_g(f, g, n):
+    if n:
+        f(n)
+        g(n)
+
+grow = lambda n: f_then_g(grow, print, n // 10)
+shrink = lambda n: f_then_g(print, shrink, n // 10)
+inverse_cascade(1234)
+
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 2) + fib(n - 1)
+
+def move_disk(disk_number, from_peg, to_peg):
+    print("Move disk" + str(disk_number) + " from peg "\
+          + str(from_peg) + " to peg " + str(to_peg) + ".")
+
+def solve_hanoi(n, start_peg, end_peg):
+    if n == 1:
+        move_disk(n, start_peg, end_peg)
+    else:
+        spare_peg = 6 - start_peg - end_peg
+        solve_hanoi(n - 1, start_peg, spare_peg)
+        move_disk(n, start_peg, end_peg)
+        solve_hanoi(n - 1, spare_peg, end_peg)
+
+
+def count_partitions(n, m):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        with_m = count_partitions(n - m, m)
+        without_m = count_partitions(n, m - 1)
+        return with_m + without_m
