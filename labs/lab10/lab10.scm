6266559d8f18e17150bd06a7c1fdf20518256b2f
Birdy <2944006086@qq.com>
2024-02-19 20:08:52 +0800

    finish lab10

--- labs/lab10/lab10.scm
+++ labs/lab10/lab10.scm
@@ -1,5 +1,10 @@
 (define (over-or-under num1 num2)
   'YOUR-CODE-HERE
+  (cond
+      ((< num1 num2) -1)
+      ((= num1 num2) 0)
+      (else 1)
+      )
 )
 
 ;;; Tests
@@ -13,6 +18,7 @@
 
 (define (make-adder num)
   'YOUR-CODE-HERE
+  (lambda (y) (+ y num))
 )
 
 ;;; Tests
@@ -23,16 +29,22 @@
 
 (define (composed f g)
   'YOUR-CODE-HERE
+  (lambda (x) (f (g x)))
 )
 
 
 (define lst
   'YOUR-CODE-HERE
+    (cons (cons 1 nil)
+        (cons 2
+              (cons (cons 3 (cons 4 nil))
+                    (cons 5 nil))))
 )
 
 
 (define (remove item lst)
   'YOUR-CODE-HERE
+  (filter (lambda (x) (not (= x item))) lst)
 )
 
 
